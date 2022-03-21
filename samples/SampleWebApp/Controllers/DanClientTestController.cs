dc239a8ffd41e357610e5994f3f4c6641029c9c8
erlend oksvoll <erlend.oksvoll@digdir.no>
2022-03-21 11:03:05 +0100

    Changed datasetrequest to list input

--- samples/SampleWebApp/Controllers/DanClientTestController.cs
+++ samples/SampleWebApp/Controllers/DanClientTestController.cs
@@ -37,8 +37,10 @@ namespace SampleWebApp.Controllers
         [ActionName("auth")]
         public async Task<ActionResult> Auth(string datasetname, string subject, [FromQuery] Dictionary<string, string> parameters)
         {
-            var dataSetRequest = GetDataSetRequest(datasetname, parameters);
-            Accreditation accreditation = await _danClient.CreateDataSetRequest(dataSetRequest, subject, "991825827");
+            var dataSetRequests = new List<DataSetRequest>();          
+            dataSetRequests.Add(GetDataSetRequest(datasetname, parameters));
+
+            Accreditation accreditation = await _danClient.CreateDataSetRequest(dataSetRequests, subject, "991825827");
 
             return Content(accreditation.ToHtmlTable(), "text/html; charset=utf-8");
         }
