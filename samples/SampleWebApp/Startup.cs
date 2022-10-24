841cdd5844198134e8aaacbb4a9e5d0430375128
Bjørn Dybvik Langfors <bdl@digdir.no>
2022-10-24 18:39:14 +0200

    Remove dependency to MaskinportenApiClient

--- samples/SampleWebApp/Startup.cs
+++ samples/SampleWebApp/Startup.cs
@@ -1,6 +1,5 @@
 using Altinn.ApiClients.Dan.Extensions;
-using Altinn.ApiClients.Dan.Models;
-using Altinn.ApiClients.Maskinporten.Config;
+using Altinn.ApiClients.Maskinporten.Extensions;
 using Altinn.ApiClients.Maskinporten.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -10,6 +9,8 @@ using Microsoft.Extensions.Hosting;
 
 namespace SampleWebApp
 {
+    public class MyClientForDan {}
+
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -21,26 +22,19 @@ namespace SampleWebApp
 
         public void ConfigureServices(IServiceCollection services)
         {
-            // Configuration for DAN (environment and subscription key)
-            services.Configure<DanSettings>(Configuration.GetSection("DanSettings"));
+            // Named
+            services.AddMaskinportenHttpClient<SettingsJwkClientDefinition>("myClientForDan", Configuration.GetSection("MaskinportenSettings"));
 
-            // Config for MaskinPorten, using a local PKCS#12 file containing private certificate for signing Maskinporten requests
-            services.Configure<MaskinportenSettings<Pkcs12ClientDefinition>>(Configuration.GetSection("MyMaskinportenSettingsForCertFile"));
-            // This registers an IDanClient for injection, see Controllers/DanClientTestController.cs for usage example
-            services.AddDanClient<Pkcs12ClientDefinition>(sp => new DanConfiguration
-            {
-                // Use Newtonsoft.Json instead of System.Text.Json
-                Deserializer = new JsonNetDeserializer()
-            });
+            services
+                .AddDanClient(Configuration.GetSection("DanSettings"))
+                .AddMaskinportenHttpMessageHandler<SettingsJwkClientDefinition>("myClientForDan");
 
-            // If the secret required for Maskinporten is found elsewhere, you can either use one of the built-in client definitions, or provide your
-            // own, like this
-            //services.Configure<MaskinportenSettings<MyCustomDanClientDefinition>>(Configuration.GetSection("MyMaskinportenSettingsForCertFile"));
-            //services.Configure<DanSettings>(Configuration.GetSection("DanSettings"));
-            //services.AddDanClient<MyCustomDanClientDefinition>();
+            // Typed
+            services.AddMaskinportenHttpClient<SettingsJwkClientDefinition, MyClientForDan>(Configuration.GetSection("MaskinportenSettings"));
 
-            // If you want to get the token from some other place, you can implementent your own IAccessTokenRetriever, and use this instead of Maskinporten. 
-            //services.AddDanClientWithAccessTokenRetriever<MyAccessTokenRetriever>();
+            services
+                .AddDanClient(Configuration.GetSection("DanSettings"))
+                .AddMaskinportenHttpMessageHandler<SettingsJwkClientDefinition, MyClientForDan>();
 
             services.AddControllers();
         }
